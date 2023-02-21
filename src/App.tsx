4adab9643b333103302c932b6c2ce6fe4356eedf
phucbndx <phucnguyendx9012@gmail.com>
2023-02-21 09:41:37 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Button, Col, Form, Input, Modal, Row } from "antd";
 import { v4 as uuidv4 } from "uuid";
 function App() {
   const [todos, setTodos] = useState<any[]>([]);
+  const [data, setData] = useState<any[]>([]);
   const [form]: any = Form.useForm();
   const [formChangeTodo]: any = Form.useForm();
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [idTodo, setIdTodo] = useState<string>("");
-
+  const getTodo: any = localStorage.getItem("todo");
+  const convert = JSON.parse(getTodo);
   const handleOk = () => {
     setIsModalOpen(false);
   };
@@ -16,29 +18,31 @@ function App() {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  const onFinish = (values: any) => {
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+  const onFinish = (values: { todo: string; id: string }) => {
     const data = {
       todo: values.todo,
       id: uuidv4(),
     };
     setTodos([...todos, data]);
+    localStorage.setItem("todo", JSON.stringify([...todos, data]));
     form.setFieldValue();
   };
   const onFinishChangeTodo = (values: { todoChange: String }) => {
-    const todoUpdate = todos.map((e) =>
+    const todoUpdate = convert.map((e: any) =>
       e.id === idTodo ? { ...e, todo: values.todoChange } : e
     );
     setIsModalOpen(false);
     setTodos(todoUpdate);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
+    localStorage.setItem("todo", JSON.stringify(todoUpdate));
   };
 
   const handleRemove = (id: string) => {
-    const todoAfterRemove = todos.filter((e) => e.id !== id);
+    const todoAfterRemove = convert.filter((e: any) => e.id !== id);
     setTodos(todoAfterRemove);
+    localStorage.setItem("todo", JSON.stringify(todoAfterRemove));
   };
 
   const handleUpdateTodo = (todo: { todo: string; id: string }) => {
@@ -49,9 +53,13 @@ function App() {
 
     setIdTodo(todo.id);
   };
-
+  useEffect(() => {
+    const getTodo: any = localStorage.getItem("todo");
+    const convert = JSON.parse(getTodo);
+    setData(convert);
+  }, [todos]);
   return (
-    <div className="App">
+    <div className="App " style={{ padding: "0px 20px" }}>
       <Row>
         <Col xs={24} md={6}></Col>
         <Col xs={24} md={12}>
@@ -79,8 +87,8 @@ function App() {
               </Form.Item>
             </Form>
           </div>
-          {todos &&
-            todos.map((todo) => {
+          {data &&
+            data.map((todo) => {
               return (
                 <div
                   key={todo.id}
